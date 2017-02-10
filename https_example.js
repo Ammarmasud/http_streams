16684f9fe85e81d39a344f91b593fff3ce8a8ddc
Ammar Masud <ammarmasud@gmail.com>
2017-02-10 22:55:43 +0000

    testing response from callback function

--- https_example.js
+++ https_example.js
@@ -1,3 +1,18 @@
 var https = require('https');
 
-console.log('I did it!');
+var options = {
+  host: 'webcache.googleusercontent.com',
+  path: '/search?q=cache:39oPFzYFz9MJ:https://example.org/&num=1&hl=en&gl=ca&strip=0&vwsrc=0'
+};
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
